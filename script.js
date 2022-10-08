80b7e355a4dec708f0e5108706fe75bc1b1a6b3a
fadayopaul <paulfadayo@yahoo.com>
2022-10-08 18:52:10 +0100

    add leaflet API

--- script.js
+++ script.js
@@ -18,7 +18,24 @@ copyDate.textContent = new Date().getUTCFullYear();
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},17z`);
+
+      const coords = [latitude, longitude];
+
+      // leaflet API
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Could not get your position");
