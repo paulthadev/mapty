6ff22766a4524a38d95800f8872bfc41aa8bba96
fadayopaul <paulfadayo@yahoo.com>
2022-10-11 11:30:46 +0100

    manage workout data by creating classes

--- script.js
+++ script.js
@@ -10,9 +10,53 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-
 const copyDate = document.querySelector(".copyright-year");
-copyDate.textContent = new Date().getUTCFullYear();
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([6.5, 3.3], 35, 23, 125);
+// const cycl1 = new Cycling([6.5, 3.3], 35, 13, 225);
+// console.log(run1, cycl1);
+
+///////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 
 class App {
   #map;
@@ -97,3 +141,6 @@ class App {
 }
 
 const app = new App();
+
+// copyright date
+copyDate.textContent = new Date().getUTCFullYear();
